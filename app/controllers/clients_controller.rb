19080874c21b0df7ad114d88d5ac7cfa0d93f9b7
Stanisław Chmiela <sjchmiela@gmail.com>
2012-07-12 15:06:04 +0200

    New and Create methods in controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -10,12 +10,21 @@ class ClientsController < ApplicationController
   end
 
   def new
+    @client = Client.new
   end
 
   def edit
   end
 
   def create
+    @client = Client.new(params[:client])
+    respond_to do |format|
+      if @client.save
+        format.html { redirect_to(@client, :notice => t("client.create.success")) }
+      else
+        format.html { render :action => "new" }
+      end
+    end
   end
 
   def update
