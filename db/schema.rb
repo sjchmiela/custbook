904e16030a20acb7ef8b843f1d72bff1fe593a3c
Stanisław Chmiela <sjchmiela@gmail.com>
2012-07-12 13:29:56 +0200

    Migrated models

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,41 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120712111252) do
+ActiveRecord::Schema.define(:version => 20120712112818) do
+
+  create_table "clients", :force => true do |t|
+    t.string   "name"
+    t.string   "surname"
+    t.string   "nid"
+    t.date     "birthday"
+    t.string   "phone"
+    t.string   "cell"
+    t.string   "email"
+    t.string   "country"
+    t.boolean  "diabetes"
+    t.string   "psoriasis"
+    t.boolean  "steroids"
+    t.boolean  "pregnancy"
+    t.boolean  "yellows"
+    t.boolean  "allergy"
+    t.boolean  "sensitive_skin"
+    t.boolean  "herpes"
+    t.string   "found"
+    t.text     "additional_info"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  create_table "treatments", :force => true do |t|
+    t.string   "type"
+    t.date     "date"
+    t.integer  "client_id"
+    t.decimal  "price"
+    t.string   "color"
+    t.boolean  "correction"
+    t.text     "additional_info"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
 end
