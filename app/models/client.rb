26f8ca492e0a4ab40b36893b82eb56765c96cffd
Stanisław Chmiela <sjchmiela@gmail.com>
2012-07-12 14:05:14 +0200

    Added update_birthday methods before save and surname validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,16 @@
 class Client < ActiveRecord::Base
   attr_accessible :additional_info, :allergy, :birthday, :cell, :country, :diabetes, :email, :found, :herpes, :name, :nid, :phone, :pregnancy, :psoriasis, :sensitive_skin, :steroids, :surname, :yellows
+  validates :surname, :presence => true
+  before_save :update_birthday
+
+  def full_name
+    return "#{self.name} #{self.surname}"
+  end
+
+  def update_birthday
+    if self.nid.size >= 6 and self.birthday == nil
+      self.birthday = Date.strptime(self.nid[0..5],"%y%m%d")
+    end
+  end
+      
 end
