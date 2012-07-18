00a37572ea361e83d68e22e188da8869fa5efbb9
Stanisław Chmiela <sjchmiela@gmail.com>
2012-07-18 18:19:27 +0200

    Added decent_exposure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 
 class PagesController < ApplicationController
-  def home
-  	@treatments_young = Treatment.find(:all, :conditions => {:date => 18.months.ago..16.months.ago})
-  	@treatments_old = Treatment.find(:all, :conditions => {:date => 30.months.ago..28.months.ago})
-  end
+  expose(:treatments_young) { Treatment.find(:all, :conditions => {:date => 18.months.ago..16.months.ago})}
+  expose(:treatments_old) {Treatment.find(:all, :conditions => {:date => 30.months.ago..28.months.ago})}
 end
