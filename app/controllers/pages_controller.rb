d0df4b1da66a0bd22cd55e7be07f5d0f3373aa98
Stanisław Chmiela <sjchmiela@gmail.com>
2012-08-14 11:25:47 +0200

    Removed N+1 problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 
 class PagesController < ApplicationController
-  expose(:treatments_young) { Treatment.find(:all, :conditions => {:date => 18.months.ago..16.months.ago})}
-  expose(:treatments_old) {Treatment.find(:all, :conditions => {:date => 30.months.ago..28.months.ago})}
+  expose(:treatments_young) { Treatment.find(:all, :conditions => {:date => 18.months.ago..16.months.ago}, :include => :client)}
+  expose(:treatments_old) {Treatment.find(:all, :conditions => {:date => 30.months.ago..28.months.ago}, :include => :client)}
 end
